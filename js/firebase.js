15d5688bb73f070cbbeba1ad37d82b66063fab3d
Yash-1-tech <131190957+Yash-1-tech@users.noreply.github.com>
2025-12-26 21:14:20 +0530

    Add Firebase configuration to firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// Firebase config (replace with your project keys)
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
